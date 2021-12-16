167bf147f9fc4ee201f63e2da53e402a64d6f82e
Zasetsky <zasetsky.evgeny@gmail.com>
2021-12-16 19:30:23 +0300

    add 'scoped' and border sale

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,7 +2,7 @@
     <div class="content">
       <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
         <div class="top-row">
-            <div class="top part">
+            <div :class="[saleBorderClass, 'top', 'part']">
               <div class="robot-name">
                 {{selectedRobot.head.title}}
                 <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
@@ -82,6 +82,14 @@ export default {
     };
   },
   computed: {
+    saleBorderClass() {
+      return this.selectedRobot.head.onSale ? 'sale-border' : '';
+    },
+    headBorderStyle() {
+      return {
+        border: this.selectedRobot.head.onSale ? '3px solid red' : '3px solid #aaa',
+      };
+    },
     selectedRobot() {
       return {
         head: availableParts.heads[this.selectedHeadIndex],
