23dc9c993b6d740c882cac5ff70b3a4621012a9e
Zasetsky <zasetsky.evgeny@gmail.com>
2022-01-06 14:59:37 +0300

    create partselector and fix exist code

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -59,27 +59,12 @@
 import availableParts from '../data/parts';
 import createdHookMixin from './created-hook-mixin';
 
-function getPreviousValidIndex(index, length) {
-  const deprecatedIndex = index - 1;
-  return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
-}
-
-function getNextValidIndex(index, length) {
-  const incrementedIndex = index + 1;
-  return incrementedIndex > length - 1 ? 0 : incrementedIndex;
-}
-
 export default {
   name: 'RobotBuilder',
   data() {
     return {
       availableParts,
       cart: [],
-      selectedHeadIndex: 0,
-      selectedLeftArmIndex: 0,
-      selectedTorsoIndex: 0,
-      selectedRightArmIndex: 0,
-      selectedBaseIndex: 0,
     };
   },
   mixins: [createdHookMixin],
@@ -108,46 +93,6 @@ export default {
       const cost = robot.head.cost + robot.leftArm.cost
       + robot.torso.cost + robot.rightArm.cost + robot.base.cost;
       this.cart.push({ ...robot, cost });
-    },
-    selectNextHead() {
-      this.selectedHeadIndex =
-       getNextValidIndex(this.selectedHeadIndex, availableParts.heads.length);
-    },
-    selectPreviousHead() {
-      this.selectedHeadIndex =
-       getPreviousValidIndex(this.selectedHeadIndex, availableParts.heads.length);
-    },
-    selectNextLeftArm() {
-      this.selectedLeftArmIndex =
-      getNextValidIndex(this.selectedLeftArmIndex, availableParts.arms.length);
-    },
-    selectPreviousLeftArm() {
-      this.selectedLeftArmIndex =
-      getPreviousValidIndex(this.selectedLeftArmIndex, availableParts.arms.length);
-    },
-    selectNextTorso() {
-      this.selectedTorsoIndex =
-      getNextValidIndex(this.selectedTorsoIndex, availableParts.torsos.length);
-    },
-    selectPreviousTorso() {
-      this.selectedTorsoIndex =
-      getPreviousValidIndex(this.selectedTorsoIndex, availableParts.torsos.length);
-    },
-    selectNextRightArm() {
-      this.selectedRightArmIndex =
-      getNextValidIndex(this.selectedRightArmIndex, availableParts.arms.length);
-    },
-    selectPreviousRightArm() {
-      this.selectedRightArmIndex =
-      getPreviousValidIndex(this.selectedRightArmIndex, availableParts.arms.length);
-    },
-    selectNextBase() {
-      this.selectedBaseIndex =
-      getNextValidIndex(this.selectedBaseIndex, availableParts.bases.length);
-    },
-    selectPreviousBase() {
-      this.selectedBaseIndex =
-      getPreviousValidIndex(this.selectedBaseIndex, availableParts.bases.length);
     },
   },
 };
